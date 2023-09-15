315c4b8a9eab660d90b85cf47a1d858c9e9f5959
Shishir <md.sabbirshishir@gmail.com>
2023-09-16 01:49:56 +0300

    modules

---
+++ Module 3.py
@@ -0,0 +1,13 @@
+#Task1
+size = float(input("Enter the length of a zander in cm: "))
+limit = (42-size)
+
+if (size >= 42):
+    print("You can keep the zander.")
+
+else:
+    print(f"The caught fish was {limit} centimeters below than the size limit")
+    print("Please release the fish back into the lake")
+
+#Task2
+
